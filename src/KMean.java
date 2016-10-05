e310750872d3785116de5428cf2dd1e8c21bd12d
drewfoglesong <dfoglesong5@gmail.com>
2016-10-04 22:29:53 -0400

    Drew's KMeans class

--- src/KMean.java
+++ src/KMean.java
@@ -1,97 +1,54 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Authors: Drew Foglesong, Daniel Lewis, Matt Taylor, Zach Litteral
+ * Date: October 6, 2016
+ * K Means Algorithm Implementation in Java
  */
-import static java.lang.Math.abs;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Random;
-import java.util.Scanner;
-
-public class KMean {
-
-    int k;
-    int noOfItems;
-    ArrayList<Integer> dataItems;
-    ArrayList<Integer> cz;
-    ArrayList<Integer> oldCz;
-    ArrayList<Integer> row;
-    ArrayList<ArrayList<Integer>> groups;
-    Scanner input;
-
-    public KMean(int k, int noOfItems) {
-        this.k = k;
-        this.noOfItems = noOfItems;
-        dataItems = new ArrayList<>();
-        cz = new ArrayList<>();
-        oldCz = new ArrayList<>();
-        row = new ArrayList<>();
-        groups = new ArrayList<>();
-        input = new Scanner(System.in);
-
-        for (int i = 0; i < k; i++) {
-            groups.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < noOfItems; i++) {
-            System.out.println("Enter Value for: " + (i + 1) + " item");
-            dataItems.add(input.nextInt());
-            if (i < k) {
-                cz.add(dataItems.get(i));
-                System.out.println("C" + (i + 1) + " is " + cz.get(i));
-            }
-        }
-        int iter = 1;
-        do {
-            for (int aItem : dataItems) {
-                for (int c : cz) {
-                    row.add(abs(c - aItem));
-                }
-                groups.get(row.indexOf(Collections.min(row))).add(aItem);
-                row.removeAll(row);
-            }
-            for (int i = 0; i < k; i++) {
-                if (iter == 1) {
-                    oldCz.add(cz.get(i));
-                } else {
-                    oldCz.set(i, cz.get(i));
-                }
-                if (!groups.get(i).isEmpty()) {
-                    cz.set(i, average(groups.get(i)));
-                }
-            }
-            if (!cz.equals(oldCz)) {
-                for (int i = 0; i < groups.size(); i++) {
-                    groups.get(i).removeAll(groups.get(i));
-                }
-            }
-            iter++;
-        } while (!cz.equals(oldCz));
-        for (int i = 0; i < cz.size(); i++) {
-            System.out.println("New C" + (i + 1) + " " + cz.get(i));
-        }
-        for (int i = 0; i < groups.size(); i++) {
-            System.out.println("Group " + (i + 1));
-            System.out.println(groups.get(i).toString());
-        }
-        System.out.println("Number of Itrations: " + iter);
-    }
-    /* Code tests that this class works on its own.
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter Number of groups to create");
-        int k = input.nextInt();
-        System.out.println("Enter No of Data Items");
-        int noOfItems = input.nextInt();
-        new KMean(k, noOfItems);
-    }*/
-
-    public static int average(ArrayList<Integer> list) {
-        int sum = 0;
-        for (Integer value : list) {
-            sum = sum + value;
-        }
-        return sum / list.size();
-    }
+import java.util.*;
+public class KMeans
+{
+	public static void main(String[] args)
+	{
+		Scanner myScanner = new Scanner(System.in);
+		System.out.print("Enter the desired number of clusters: ");
+		int numClusters = Integer.parseInt(myScanner.nextLine());
+	}
+	
+	private static class Student
+	{
+		private int mathScore, satScore, gender, financialSupport;
+		
+		public Student(int mathSchore, int satScore, int gender, int financialSupport)
+		{
+			this.mathScore = mathScore;
+			this.satScore = satScore;
+			this.gender = gender;
+			this.financialSupport = financialSupport;
+		}
+		
+		public static Student[] initializeStudents()
+		{
+			Student[] students = new Student[300];
+			Random generator = new Random(123);
+			for(int i = 0; i < students.length; i++)
+			{
+				int mathScore = generator.nextInt(100);
+				int satScore = generator.nextInt(600 + 1) + 200;
+				int gender = generator.nextInt(1);
+				int financialSupport = generator.nextInt(9500) + 500;
+				Student newStudent = new Student(mathScore, satScore, gender, financialSupport);
+				students[i] = newStudent;
+			}
+			return students;
+		}
+		
+		public static double distance(Student studentA, Student studentB)
+		{
+			return Math.sqrt(Math.pow((studentA.mathScore - studentB.mathScore), 2) + Math.pow((studentA.satScore - studentB.satScore), 2) + Math.pow((studentA.gender - studentB.gender), 2) + Math.pow((studentA.financialSupport - studentB.financialSupport), 2));
+		}
+		
+		public static Student getCentroid(ArrayList<Student> studentList)
+		{
+			
+		}
+	}
 }
